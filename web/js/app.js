62248f87178c0b8e265da2a9dc7cbcebb2c3901a
Tyler Jones <tyler.jones@coyotes.usd.edu>
2014-10-03 01:05:16 -0500

    Added search, panels, factory service
    Added a factory service to pass data to scoped controller.
    
    Started JSON for the data tree.

--- web/js/app.js
+++ web/js/app.js
@@ -1,13 +1,15 @@
-(function () {
-    var app = angular.module('BTL', ['angularTreeview','angucomplete']);
-    app.directive('draggable', function () {
-        return function (scope, element) {
+(function() {
+    var app = angular.module('BTL', ['angularTreeview']);
+
+
+    app.directive('draggable', function() {
+        return function(scope, element) {
             // this gives us the native JS object
             var el = element[0];
             el.draggable = true;
             el.addEventListener(
                     'dragstart',
-                    function (e) {
+                    function(e) {
                         e.dataTransfer.effectAllowed = 'move';
                         e.dataTransfer.setData('Text', this.id);
                         this.classList.add('drag');
@@ -17,7 +19,7 @@
                     );
             el.addEventListener(
                     'dragend',
-                    function (e) {
+                    function(e) {
                         this.classList.remove('drag');
                         return false;
                     },
@@ -25,17 +27,17 @@
                     );
         }
     });
-    app.directive('droppable', function () {
+    app.directive('droppable', function() {
         return {
             scope: {
                 drop: '&' // parent
             },
-            link: function (scope, element) {
+            link: function(scope, element) {
                 // again we need the native object
                 var el = element[0];
                 el.addEventListener(
                         'dragover',
-                        function (e) {
+                        function(e) {
                             e.dataTransfer.dropEffect = 'move';
                             // allows us to drop
                             if (e.preventDefault)
@@ -50,7 +52,7 @@
                         );
                 el.addEventListener(
                         'dragenter',
-                        function (e) {
+                        function(e) {
                             this.classList.add('over');
                             return false;
                         },
@@ -58,13 +60,13 @@
                         );
                 el.addEventListener(
                         'dragleave',
-                        function (e) {
+                        function(e) {
                             this.classList.remove('over');
                             return false;
                         },
                         false
                         );
-                el.addEventListener('drop', function (e) {
+                el.addEventListener('drop', function(e) {
                     // Stops some browsers from redirecting.
                     if (e.stopPropagation)
                         e.stopPropagation();
@@ -106,198 +108,65 @@
             }
         } //end return
     });
-    app.controller('ToolListController', function ($scope, $http) {
-        $scope.toolVal = {version: '1.0'};
-        getBets($scope, $http);
-        getToolList($scope, $http);
-        getReporting($scope, $http);
-        $scope.handleDrop = function () {
-            //code can go here
-        };
-    }); // END ToolListController 
 
-    app.controller('ToolTreeController', function ($scope) {
+    app.factory('Data', function() {
+        return {message: "I'm data from a service"}
+    });
 
-        $scope.nameFilter = null;
-        $scope.searchFilter = function (driver) {
-            var keyword = new RegExp($scope.nameFilter, 'i');
-            return !$scope.nameFilter || keyword.test(driver.Driver.givenName) || keyword.test(driver.Driver.familyName);
-        };
-        //test tree data
-        $scope.treeList = [
-            {"roleName": "Applications", "id": "role1", "children": [
-                    {"roleName": "subApplication", "id": "role11", "collapsed": true, "children": [
-                            {//child tool object 
-                                "roleName": "Test Tool 1",
-                                "id": 001,
-                                "toolName": "Test Tool 1",
-                                "toolDescription": "Test tool 1 description",
-                                "domains": [
-                                    {
-                                        "domainName": "Tool 1 Domain Name",
-                                        "domainURI": "http://domain.uri1"
-                                    }
-                                ],
-                                "applications": [
-                                    {
-                                        "appName": "Tool 1 Application Name",
-                                        "appURI": "http://app.uri1"
-                                    }
-                                ]
-                            },
-                            {
-                                "roleName": "Test Tool 2",
-                                "id": 002,
-                                "toolName": "Test Tool 2",
-                                "toolDescription": "Test tool 2 description",
-                                "domains": [
-                                    {
-                                        "domainName": "Tool 2 Domain Name",
-                                        "domainURI": "http://domain.uri2"
-                                    }
-                                ],
-                                "applications": [
-                                    {
-                                        "appName": "Tool 2 Application Name",
-                                        "appURI": "http://app.uri2"
-                                    }
-                                ]
-                            }
-                        ]},
-                    {"roleName": "subApplication2", "id": "role12", "collapsed": true, "children": [
-                            {"roleName": "subApplication2-1", "id": "role121", "children": [
-                                    {"roleName": "subApplication2-1-1", "id": "role1211", "children": []},
-                                    {"roleName": "subApplication2-1-2", "id": "role1212", "children": [
-                                            {"roleName": "subApplication2-1-2-1", "id": "role1211", "children": [
-                                                    {"roleName": "subApplication2-1-1", "id": "role1211", "children": []}
-                                                ]},
-                                            {"roleName": "subApplication2-1-2-2", "id": "role1211", "children": []}
-                                        ]}
-                                ]}
-                        ]}
-                ]},
-            {"roleName": "Domains", "id": "role2", "children": [
-                    {"roleName": "subDomain1", "id": "role11", "collapsed": true, "children": []},
-                    {"roleName": "subDomain2", "id": "role12", "children": [
-                            {"roleName": "subDomain2-1", "id": "role121", "children": [
-                                    {"roleName": "subDomain2-1-1", "id": "role1211", "children": []},
-                                    {"roleName": "subDomain2-1-2", "id": "role1212", "children": []}
-                                ]}
-                        ]}
-                ]},
-            {"roleName": "Methods", "id": "role3", "children": [
-                    {"roleName": "subGuest1", "id": "role11", "children": []},
-                    {"roleName": "subGuest2", "id": "role12", "collapsed": true, "children": [
-                            {"roleName": "subGuest2-1", "id": "role121", "children": [
-                                    {
-                                        "roleName": "Test Tool 1",
-                                        "id": 001,
-                                        "toolName": "Test Tool 1",
-                                        "toolDescription": "Test tool 1 description",
-                                        "domains": [
-                                            {
-                                                "domainName": "Tool 1 Domain Name",
-                                                "domainURI": "http://domain.uri1"
-                                            }
-                                        ],
-                                        "applications": [
-                                            {
-                                                "appName": "Tool 1 Application Name",
-                                                "appURI": "http://app.uri1"
-                                            }
-                                        ]
-                                    },
-                                    {
-                                        "roleName": "Test Tool 2",
-                                        "id": 002,
-                                        "toolName": "Test Tool 2",
-                                        "toolDescription": "Test tool 2 description",
-                                        "domains": [
-                                            {
-                                                "domainName": "Tool 2 Domain Name",
-                                                "domainURI": "http://domain.uri2"
-                                            }
-                                        ],
-                                        "applications": [
-                                            {
-                                                "appName": "Tool 2 Application Name",
-                                                "appURI": "http://app.uri2"
-                                            }
-                                        ]
-                                    }
-                                ]}
-                        ]}
-                ]}
-        ];
+    app.controller('ToolTreeController', function($scope, $http) {
+        $http.get("./input_files/reporting_file.json")
+                .success(function(response) {
+                    $scope.treeList = response;
+                });
+    });
+
+    app.controller('DataTreeController', function($scope, $http) {
+        $http.get("./input_files/dataTree.json")
+                .success(function(response) {
+                    $scope.dataTree = response;
+                });
+    });
+
+    app.controller('SimilarToolController', function($scope, Data) { //data is injected from app.factory 'Data' service
+        $scope.data = Data;
     });
 
 
-    app.controller('ReportingController', function ($scope, $filter) {
 
-        var tools = {
-            "tools":
-                    [
-                        {
-                            "toolId": 001,
-                            "toolName": "Test Tool 1",
-                            "toolDescription": "Test tool 1 description",
-                            "domains": [
-                                {
-                                    "domainName": "Tool 1 Domain Name",
-                                    "domainURI": "http://domain.uri1"
-                                }
-                            ],
-                            "applications": [
-                                {
-                                    "appName": "Tool 1 Application Name",
-                                    "appURI": "http://app.uri1"
-                                }
-                            ]
-                        },
-                        {
-                            "toolId": 002,
-                            "toolName": "Test Tool 2",
-                            "toolDescription": "Test tool 2 description",
-                            "domains": [
-                                {
-                                    "domainName": "Tool 2 Domain Name",
-                                    "domainURI": "http://domain.uri2"
-                                }
-                            ],
-                            "applications": [
-                                {
-                                    "appName": "Tool 2 Application Name",
-                                    "appURI": "http://app.uri2"
-                                }
-                            ]
-                        }
-                    ]
-        };
-        var result = $filter('filter')(tools.tools, {toolId: 002})[0];
-        $scope.name = result.toolName;
-        $scope.tools = tools;
-    });
+
+
+//    app.controller('ToolListController', function($scope, $http) {
+//        $scope.toolVal = {version: '1.0'};
+//        getBets($scope, $http);
+//        getToolList($scope, $http);
+//        getReporting($scope, $http);
+//        $scope.handleDrop = function() {
+//            //code can go here
+//        };
+//    }); // END ToolListController 
+
+
     /**
      * 
      HELPER METHODS
      */
-    function getBets($scope, $http) {
-        $http.get('input_files/test_bets.json').success(function (data) {
-            $scope.toolData = data;
-        });
-    }
-    ;
-    function getToolList($scope, $http) {
-        $http.get('input_files/tool_list.json').success(function (data) {
-            $scope.toolList = data;
-        });
-    }
-    ;
-    function getReporting($scope, $http) {
-        $http.get('input_files/reporting_file.json').success(function (data) {
-            $scope.report = data;
-        });
-    }
-    ;
+//    function getBets($scope, $http) {
+//        $http.get('input_files/test_bets.json').success(function(data) {
+//            $scope.toolData = data;
+//        });
+//    }
+//    ;
+//    function getToolList($scope, $http) {
+//        $http.get('input_files/tool_list.json').success(function(data) {
+//            $scope.toolList = data;
+//        });
+//    }
+//    ;
+//    function getReporting($scope, $http) {
+//        $http.get('input_files/reporting_file.json').success(function(data) {
+//            $scope.report = data;
+//        });
+//    }
+//    ;
 })();
     
--- web/js/treeview.js
+++ web/js/treeview.js
@@ -1,7 +1,9 @@
 (function(l) {
     l.module("angularTreeview", []).directive("treeModel", function($compile) {
         return{restrict: "A", link: function(a, g, c) {
-                var e = c.treeModel, h = c.nodeLabel || "label", d = c.nodeChildren || "children", k = '<ul><li data-ng-repeat="node in ' + e + '"><i class="collapsed" data-ng-show="node.' + d + '.length && node.collapsed" data-ng-click="selectNodeHead(node, $event)"></i><i class="expanded" data-ng-show="node.' + d + '.length && !node.collapsed" data-ng-click="selectNodeHead(node, $event)"></i><i class="normal" data-ng-hide="node.' +
+                l.search = null;
+
+                var e = c.treeModel, h = c.nodeLabel || "label", d = c.nodeChildren || "children", k = '<ul><li data-ng-repeat="node in ' + e + '| filter:search"><i class="collapsed" data-ng-show="node.' + d + '.length && node.collapsed" data-ng-click="selectNodeHead(node, $event)"></i><i class="expanded" data-ng-show="node.' + d + '.length && !node.collapsed" data-ng-click="selectNodeHead(node, $event)"></i><i class="normal" data-ng-hide="node.' +
                         d + '.length"></i> <span data-ng-class="node.selected" data-ng-click="selectNodeLabel(node, $event)">{{node.' + h + '}}</span><div data-ng-hide="node.collapsed" data-tree-model="node.' + d + '" data-node-id=' + (c.nodeId || "id") + " data-node-label=" + h + " data-node-children=" + d + "></div></li></ul>";
                 e && e.length && (c.angularTreeview ? (a.$watch(e, function(m, b) {
                     g.empty().html($compile(k)(a))
@@ -22,5 +24,5 @@
                     a.currentNode = c
                 }) : g.html($compile(k)(a)))
             }}
-    })
+    });
 })(angular);
