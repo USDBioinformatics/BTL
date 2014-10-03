bda632a1e4a9999acab070572921399033bfc052
tjones788 <tyler.jones@coyotes.usd.edu>
2014-10-03 16:53:50 -0500

    Changes

--- web/js/treeview.js
+++ web/js/treeview.js
@@ -2,7 +2,7 @@
     l.module("angularTreeview", []).directive("treeModel", function($compile) {
         return{restrict: "A", link: function(a, g, c) {
                 l.search = null;
-
+                
                 var e = c.treeModel, h = c.nodeLabel || "label", d = c.nodeChildren || "children", k = '<ul><li draggable data-ng-repeat="node in ' + e + '| filter:search"><i class="collapsed" data-ng-show="node.' + d + '.length && node.collapsed" data-ng-click="selectNodeHead(node, $event)"></i><i class="expanded" data-ng-show="node.' + d + '.length && !node.collapsed" data-ng-click="selectNodeHead(node, $event)"></i><i class="normal" data-ng-hide="node.' +
                         d + '.length"></i> <span data-ng-class="node.selected" data-ng-click="selectNodeLabel(node, $event)">{{node.' + h + '}}</span><div data-ng-hide="node.collapsed" data-tree-model="node.' + d + '" data-node-id=' + (c.nodeId || "id") + " data-node-label=" + h + " data-node-children=" + d + "></div></li></ul>";
                 e && e.length && (c.angularTreeview ? (a.$watch(e, function(m, b) {
@@ -23,6 +23,6 @@
                     c.selected = "selected";
                     a.currentNode = c
                 }) : g.html($compile(k)(a)))
-            }}
+            }};
     });
 })(angular);
