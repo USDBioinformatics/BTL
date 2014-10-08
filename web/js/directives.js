1b4ad4e39cdebabff4faa4154a964b4771728823
tjones788 <tyler.jones@coyotes.usd.edu>
2014-10-08 16:39:32 -0500

    test

--- web/js/directives.js
+++ web/js/directives.js
@@ -1,38 +1,5 @@
 var sharedDirectives = angular.module("Directives", []);
 
-sharedDirectives.directive("treeModel", function ($compile) {
-
-    return{
-        restrict: "A",
-        link: function (a, g, c) {
-            var e = c.treeModel;
-            var h = c.nodeLabel || "label";
-            var d = c.nodeChildren || "children";
-            var k = '<ul><li draggable data-ng-repeat="node in ' + e + '| filter:search"><i class="collapsed" data-ng-show="node.' + d + '.length && node.collapsed" data-ng-click="selectNodeHead(node, $event)"></i><i class="expanded" data-ng-show="node.' + d + '.length && !node.collapsed" data-ng-click="selectNodeHead(node, $event)"></i><i class="normal" data-ng-hide="node.' +
-                    d + '.length"></i> <span  data-ng-class="node.selected" data-ng-click="selectNodeLabel(node, $event)">{{node.' + h + '}}</span><div data-ng-hide="node.collapsed" data-tree-model="node.' + d + '" data-node-id=' + (c.nodeId || "id") + " data-node-label=" + h + " data-node-children=" + d + "></div><span class='toolCt'>#</span></li></ul>";
-
-
-            e && e.length && (c.angularTreeview ? (a.$watch(e, function (m, b) {
-                g.empty().html($compile(k)(a));
-            }, !1),
-                    a.selectNodeHead = a.selectNodeHead || function (a, b) {
-                        b.stopPropagation && b.stopPropagation();
-                        b.preventDefault && b.preventDefault();
-                        b.cancelBubble =
-                                !0;
-                        b.returnValue = !1;
-                        a.collapsed = !a.collapsed;
-                    }, a.selectNodeLabel = a.selectNodeLabel || function (c, b) {
-                b.stopPropagation && b.stopPropagation();
-                b.preventDefault && b.preventDefault();
-                b.cancelBubble = !0;
-                b.returnValue = !1;
-                a.currentNode && a.currentNode.selected && (a.currentNode.selected = void 0);
-                c.selected = "selected";
-                a.currentNode = c;
-            }) : g.html($compile(k)(a)));
-        }};
-});
 
 sharedDirectives.directive('draggable', function () {
     return function (element) {
