a27945e08986218bb2e4191deca4471ca89c06c8
Tyler Jones <tyler.jones@coyotes.usd.edu>
2014-10-13 23:52:47 -0500

    Working on similartoolcontroller

--- web/js/app.js
+++ web/js/app.js
@@ -17,7 +17,12 @@
                         return $scope.state;
                     };
                 }, link: function (b, h, c) {
-                    var a = c.treeId, g = c.treeModel, e = c.nodeLabel || "label", d = c.nodeChildren || "children", e = '<ul><li data-ng-repeat="node in ' + g + '"><i class="collapsed" data-ng-show="node.' + d + '.length && node.collapsed" data-ng-click="' + a + '.selectNodeHead(node)"></i><i class="expanded" data-ng-show="node.' + d + '.length && !node.collapsed" data-ng-click="' + a + '.selectNodeHead(node)"></i><i class="normal" data-ng-hide="node.' +
+                    var a = c.treeId;
+                    var g = c.treeModel;
+                    var e = c.nodeLabel || "label";
+                    var d = c.nodeChildren || "children";
+                    
+                    e = '<ul><li data-ng-repeat="node in ' + g + '"><i class="collapsed" data-ng-show="node.' + d + '.length && node.collapsed" data-ng-click="' + a + '.selectNodeHead(node)"></i><i class="expanded" data-ng-show="node.' + d + '.length && !node.collapsed" data-ng-click="' + a + '.selectNodeHead(node)"></i><i class="normal" data-ng-hide="node.' +
                             d + '.length"></i> <span data-ng-class="node.selected" data-ng-click="' + a + '.selectNodeLabel(node)">{{node.' + e + '}}</span><div data-ng-hide="node.collapsed" data-tree-id="' + a + '" data-tree-model="node.' + d + '" data-node-id=' + (c.nodeId || "id") + " data-node-label=" + e + " data-node-children=" + d + "></div></li></ul>";
                     a && g && (c.angularTreeview && (b[a] = b[a] || {}, b[a].selectNodeHead = b[a].selectNodeHead || function (a) {
                         a.collapsed = !a.collapsed
--- web/js/controllers.js
+++ web/js/controllers.js
@@ -2,60 +2,60 @@ var controllers = angular.module("Controllers", []);
 
 controllers.controller('GlobalController', function($scope) { //pass Tree factory
     $scope.selectedNode = {};
-    $scope.selectedNode.id;
-
+    $scope.toolInputs = {
+        "format": []
+    };
 });
 
 controllers.controller('ToolTreeController', function($scope, TreeListService) { //pass Tree factory
     TreeListService.success(function(data) {
         $scope.treeList = data;
     });
-
-    $scope.$watch('toolTree.currentNode', function(newObj, oldObj) {
+    $scope.$watch('toolTree.currentNode', function() {
+        var curNode;
         if ($scope.toolTree && angular.isObject($scope.toolTree.currentNode)) {
-            $scope.selectedNode.id = $scope.toolTree.currentNode.id;
-        };
-//            console.log('Node Selected!!');
-//            console.log($scope.toolTree.currentNode);
-//            
-//            
-////            $scope.setToolId = function(newValue) {
-////                $scope.objectValue.data = newValue;
-////                sharedData.setToolId(newValue);
-////            };
-//        }
-//    }, false);
+            curNode = $scope.toolTree.currentNode;
+            $scope.selectedNode.id = curNode.id;
+            $scope.selectedNode.name = curNode.roleName;
+            $scope.selectedNode.children = curNode.children;
+            $scope.toolInputs.format.length = 0; //reset inputs array
 
-//    $scope.objectValue = sharedData.getObject();
-
-    });
+            if (curNode.isTool) {
+                alert('IS A TOOL');
+                var inputCt = curNode.inputs.length;
+                for (i = 0; i < inputCt; i++) {
+                    $scope.toolInputs.format.push(curNode.inputs[i].format);
+                    $scope.selectedNode.isTool = true;
+                    alert($scope.toolInputs.format.length);
+                }
+            }else{
+                $scope.selectedNode.isTool = false;
+            }
+            
+        }
+    }); //End currentNode $watch
 });
-
 controllers.controller('ToolDataController', function($scope, TreeListService, sharedData) {
-    TreeListService.success(function(data) {
-        $scope.toolId = sharedData.getToolId();
-    });
+    $scope.toolId = sharedData.getToolId();
 });
-
 controllers.controller('DataTreeController', function($scope, DataTreeService, sharedData) {
     DataTreeService.success(function(data) {
         $scope.dataTree = data;
         $scope.toolId = sharedData.getToolId();
     });
 });
-
 controllers.controller('SimilarToolController', function($scope, SimilarToolService, sharedData) { //data is injected from app.factory 'Data' service
     SimilarToolService.success(function(data) {
         $scope.similarToolList = data;
     });
+    $scope.$watch('toolInputs.count', function(newObj, oldObj) {
+        alert("watched in similarTool");
+        $scope.similarTools = [];
+    });
 });
-
 controllers.controller('PrevNextToolController', function($scope, sharedData) { //data is injected from app.factory 'Data' service
     $scope.toolId = sharedData.getToolId();
 });
-
-
-
 //app.controller('BookController', ['$scope', '$http', function($scope, $http) {
 //    var bookId = 1;
 //
