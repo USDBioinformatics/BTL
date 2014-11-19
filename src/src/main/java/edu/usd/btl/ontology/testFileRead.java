1687a71de73085fdb5f8b33f9af825631ac40d2d
tjones788 <tyler.jones@coyotes.usd.edu>
2014-11-19 02:33:45 -0600

    Ignored testFileRead
    Kept having to change local EDAM.owl file location

--- src/src/main/java/edu/usd/btl/ontology/testFileRead.java
+++ src/src/main/java/edu/usd/btl/ontology/testFileRead.java
@@ -18,8 +18,8 @@ public class testFileRead {
     public ArrayList readOntologyFile() {
         //read owl ontology file
         OntologyFileRead ontFileRead = new OntologyFileRead();
-        ArrayList<BioPortalElement> nodeList = ontFileRead.readFile("C:\\Users\\Tyler\\Documents\\GitHub\\BTL\\src\\ontology_files\\EDAM_1.3.owl");
-        //ArrayList<BioPortalElement> nodeList = ontFileRead.readFile("C:\\Users\\tyler.jones\\Desktop\\GitHub\\BTL\\src\\ontology_files\\EDAM_1.3.owl");
+        //ArrayList<BioPortalElement> nodeList = ontFileRead.readFile("C:\\Users\\Tyler\\Documents\\GitHub\\BTL\\src\\ontology_files\\EDAM_1.3.owl");
+        ArrayList<BioPortalElement> nodeList = ontFileRead.readFile("C:\\Users\\tyler.jones\\Desktop\\GitHub\\BTL\\src\\ontology_files\\EDAM_1.3.owl");
         //ArrayList<BioPortalElement> nodeList = ontFileRead.readFile("C:\\Users\\Tyler_000\\Desktop\\GitHub\\BTL\\src\\ontology_files\\EDAM_1.3.owl");
 
 //print
